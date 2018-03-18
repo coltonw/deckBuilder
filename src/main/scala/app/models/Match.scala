e956c6a15fccaf98f2a7f8982cd329f09d0ac386
Will Colton <coltonw@gmail.com>
2018-03-18 12:18:53 -0400

    Added win condition.

--- src/main/scala/app/models/Match.scala
+++ src/main/scala/app/models/Match.scala
@@ -1,9 +1,16 @@
 package app.models
 
 case class Match(deck: Set[Card], enemyDeck: Set[Card]) {
-  val myBreakdown = Breakdown(deck)
+  val myBreakdown    = Breakdown(deck)
   val enemyBreakdown = Breakdown(enemyDeck)
-  val myScore = deck.foldLeft(0d) { (score, card) =>
-    score + card.power(myBreakdown.exclude(card), enemyBreakdown)
+  val myScore        = Match.score(deck, myBreakdown, enemyBreakdown)
+  val enemyScore     = Match.score(enemyDeck, enemyBreakdown, myBreakdown)
+  val win            = myScore > enemyScore
+}
+
+object Match {
+  def score(myDeck: Set[Card], myBreakdown: Breakdown, enemyBreakdown: Breakdown): Double = myDeck.foldLeft(0d) {
+    (score, card) =>
+      score + card.power(myBreakdown.exclude(card), enemyBreakdown)
   }
 }
